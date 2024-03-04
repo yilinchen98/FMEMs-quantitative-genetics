a2d68ed967fc414f29ad6ce6a80d4135376def34
Yilin Chen <72085413+yilinchen98@users.noreply.github.com>
2024-03-04 14:17:22 +0000

    Update convert_to_basisfunctions.R

--- R_code/convert_to_basisfunctions.R
+++ R_code/convert_to_basisfunctions.R
@@ -3,26 +3,51 @@ convert_to_basisfunctions <- function(t, eigenvecs, tout, method = "linear")
   #'This function converts an eigenvector to an eigenfunction using interpolation.
   #'
   #'@param t a numeric vector containing the fine time grid points used to compute the principal components.
-  #'@param eigenvecs matrix of eigenvectors obtained from FPCA.
+  #'@param eigenvecs vector or matrix of eigenvectors obtained from FPCA.
   #'@param tout an optional set of numeric values specifying where interpolation is to take place.
   #'@param method specifies the interpolation method to be used. Choices are "linear" or "spline".
   #'@return matrix where each column represents an eigenfunction of time.
   
   
   # Initialize an empty matrix to store eigenfunctions
-  eigen_functions <- matrix(0, nrow = length(tout), ncol = ncol(eigenvecs))
   
-  # Interpolate eigenvectors to the original time points
-  for (i in 1:ncol(eigenvecs)) {
-    if (method == "linear"){
-      eigen_functions[,i] <- approx(x = t, y = eigenvecs[,i], xout = tout)$y
+  if (is.vector(eigenvecs) == TRUE){
+   
+    if (method =="linear"){
+
+      for (i in 1: length(eigenvecs)){
+        eigen_functions <- approx(x = t, y = eigenvecs, xout = tout)$y
+      }
     }
     else if (method == "spline"){
-      eigen_functions[,i] <- spline(x = t, y = eigenvecs[,i], xout = tout, method="natural")$y
+      for (i in 1: length(eigenvecs)){
+        eigen_functions <- spline(x = t, y = eigenvecs, xout = tout, method = "natural")$y
+      }
     }
-    else {
+    else{
       stop("Invalid method. Supported methods are 'linear' and 'spline'.")
     }
   }
+  
+  else{
+    eigen_functions <- matrix(0, nrow = length(tout), ncol = ncol(eigenvecs))
+    
+    for (i in 1:ncol(eigenvecs)) {
+      
+      if (method == "linear"){
+        eigen_functions[,i] <- approx(x = t, y = eigenvecs[,i], xout = tout)$y
+      }
+      else if (method == "spline"){
+        eigen_functions[,i] <- spline(x = t, y = eigenvecs[,i], xout = tout, method="natural")$y
+      }
+      else {
+        stop("Invalid method. Supported methods are 'linear' and 'spline'.")
+      }
+    }
+  }
+  
+  # Interpolate eigenvectors to the original time points
+    
   return(eigen_functions)
+
 }
