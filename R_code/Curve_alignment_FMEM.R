031c3f1e51759c56dd2cd414ef1e8466c3e418fb
yilinchen98 <chenyilinxy98@gmail.com>
2024-05-20 17:43:47 +0100

    compute joint FPCA and re-form the dataframe for model fitting.

--- R_code/Curve_alignment_FMEM.R
+++ R_code/Curve_alignment_FMEM.R
@@ -10,7 +10,6 @@ library(pedigreemm)
 library(mvnfast)
 library(ggplot2)
 library(plotly)
-library(GeodRegr)
 
 ## Import data and rescale to unit interval
 setwd("D:/KCL_2023-2027_PhD/Genetics_FMEM_Project/FMEMs-quantitative-genetics/R_code")
@@ -64,50 +63,72 @@ for (i in 1: length(large_lam)){
 ## Curve alignment
 ### use fdasrvf to compute the aligned functions and warping functions
 aligned_mass_process <- time_warping(f=trait_hat, time=agefine)
-aligned_mass_curve <- aligned_mass_process$fn # aligned function (amplitude function)
+aligned_mass_curve <- aligned_mass_process$fn # aligned curves (amplitude function)
+qn <- aligned_mass_process$qn # aligned curves in SRVF space
 aligned_mean <- aligned_mass_process$fmean
 warping_funs <- aligned_mass_process$warping_functions # warping function
 
-psi <- SqrtMean(warping_funs)$psi # square root of warping functions
-gam_mu <- SqrtMean(warping_funs)$gam_mu # center of warping functions Karcher mean 
-
-mu <- SqrtMean(warping_funs)$mu # Karcher mean of psi
-plot(agefine, gam_mu, type = "l") # identity map
-grid(nx = NULL, ny = NULL,lty = 2,col = "gray",lwd = 2)
-
 ## Compute phase function
-x <- gam_to_v(warping_funs) #map warping function to tangent space at identity
-
-### jointFPCA
-
 jointFPCA_process <- jointFPCA(aligned_mass_process, no = 3)
-eigen_vec <- jointFPCA_process$U
-C <- jointFPCA_process$C
+C <- jointFPCA_process$C # scaling coefficient
+psi <- SqrtMean(warping_funs)$psi # srvf of warping functions
+v <- SqrtMean(warping_funs)$vec #phase function = shooting vector
+
+# The eigen vector extracted from the function joinFPCA
+# it joins the qn (srvf of original curves) and the warping functions
+
+eigen_vec <- jointFPCA_process$U # eigen vector
 
 par(mfrow = c(3,1))
-plot(seq(0,1, length=201), eigen_vec[,1], type = "l")
-plot(seq(0,1, length=201), eigen_vec[,2], type = "l")
-plot(seq(0,1, length=201), eigen_vec[,3], type = "l")
+for (i in 1:3) {
+  plot(seq(0,1,length=201), eigen_vec[, i], type = "l", 
+       xlab = "time", ylab = "",
+       main = paste(" Eigen Vector", i))
+}
 
-eigen_vec[,2] %*% eigen_vec[,3]
+g2 <- rbind(qn, C*v)
 
-############################
-g <- rbind(aligned_mass_curve, C*x)
-
-fpcaobj_comb <- prcomp(x=t(g), retx = TRUE, center = TRUE, rank. = 3)
-eigen_comb <- fpcaobj_comb$rotation # eigen vectors
-
+comb_pca <-  prcomp(x=t(g2), retx = TRUE, center = TRUE, rank. = 3)$rotation
 par(mfrow=c(3,1))
 for (i in 1:3) {
-  plot(seq(0,1,length=200), eigen_comb[, i], type = "l", 
+  plot(seq(0,1,length=200), comb_pca[, i], type = "l", 
        xlab = "time", ylab = "",
        main = paste(" Combined Principal Component", i))
 }
 
-eigen_comb[,1] %*% eigen_comb[,2]
+
+### joint FPCA
+#jointFPCA_process <- jointFPCA(aligned_mass_process, no = 3)
+#C <- jointFPCA_process$C # scaling coefficient
+g <- rbind(aligned_mass_curve, C*v) # joint aligned curves (amplitude functions) and corresponding phase function
+
+Joint_fpcaobj <- prcomp(x=t(g), retx = TRUE, center = TRUE, rank. = 3)
+joint_eig_vecs <- Joint_fpcaobj$rotation # eigen vectors
+par(mfrow=c(3,1))
+for (i in 1:3) {
+  plot(seq(0,1,length=200), joint_eig_vecs[, i], type = "l", 
+       xlab = "time", ylab = "",
+       main = paste(" Joint Principal Component", i))
+}
+
+joint_eig_vecs[,1] %*% joint_eig_vecs[,2]
+joint_eig_vecs[,1] %*% joint_eig_vecs[,3]
+joint_eig_vecs[,3] %*% joint_eig_vecs[,2]
+
+fpcaobj_mass <- prcomp(x=t(aligned_mass_curve), retx = TRUE, center = TRUE) 
+eigen_mass <- fpcaobj_mass$rotation # eigen vectors
+
+## Visualise the first three eigenfunctions
+par(mfrow=c(3,1))
+for (i in 1:3) {
+  matplot(agefine, eigen_mass[, i], type = "l", 
+          xlab = "time", ylab = "",
+          main = paste("Growth Principal Component", i))
+}
 
 ## Fit mixed-effect model
 
+### Compute the genetic relationship matrix A
 pos = df$id[FirstUniqueIdPos] # extract ids for all subjects
 sire_id = df$sire[FirstUniqueIdPos] # extract ids for sire
 dam_id = df$dam[FirstUniqueIdPos] # extract ids for dam
@@ -116,22 +137,50 @@ pede <- editPed(sire = sire_id, dam = dam_id, label = pos)
 ped<- with(pede, pedigree(label=label, sire=sire, dam=dam))
 A <- getA(ped)[163:1035,163:1035]
 
-## fit smoothed data at original time points
-trait_hat_list <- list()
-
+### interpolate smoothed data, corresponding warping functions and the joint PCs at original time points
+trait_hat_list <- list() # smoothed data
 for (i in 1:N){
   inter_trait <- convert_to_basisfunctions(t = agefine, eigenvecs = trait_hat[,i],
                                            tout = age_list_new[[i]])
   trait_hat_list[[i]] <- inter_trait
 }
 
-phasefun_list <- list()
-
+phasefun_list <- list() # shooting vectors (srvf transformed warping functions)
 for(i in 1:N){
-  inter_phase <- convert_to_basisfunctions(t=agefine, eigenvecs = C*x[,i], 
+  inter_phase <- convert_to_basisfunctions(t=agefine, eigenvecs = C*v[,i], 
                                            tout = age_list_new[[i]])
   phasefun_list[[i]] <- inter_phase
 }
+
+basisfun_list <- list() # joint PCs
+for (i in 1:N){
+  PCs_curve <- convert_to_basisfunctions(t = agefine, eigenvecs = joint_eig_vecs[1:100,],
+                                   tout = age_list_new[[i]])
+  PCs_warping <- convert_to_basisfunctions(t = agefine, eigenvecs = joint_eig_vecs[101:200,],
+                                           tout = age_list_new[[i]])
+  basisfun_list[[i]] <- rbind(PCs_curve,PCs_warping)
+}
+
+### re-arrange the smoothed data, warping funtions and joint PCs into a dataframe
+smoothed_curve <- list()
+for (i in 1:N){
+  curve_data <- c(trait_hat_list[[i]], phasefun_list[[i]]) # combined smoothed curve data and warping functions (shooting vector)
+  smoothed_curve[[i]] <- curve_data
+}
+
+id_list <- list()
+for (i in 1:N){
+  id <- rep(pos[i], length(smoothed_curve[[i]])) # reform the id 
+  id_list[[i]] <- id
+}
+
+subject_id <- unlist(id_list) 
+trait <- unsplit(smoothed_curve, subject_id)
+basis <- do.call(rbind,basisfun_list)
+colnames(basis) <- c("phi1", "phi2", "phi3")
+
+df_model <- data.frame(id = subject_id, trait = trait, basis) # this is new dataframe
+
 
 
 
